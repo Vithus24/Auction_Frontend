8d4099b62aac83aac310b8b9794716199dcb9ccf
Vithus24 <vithus24082000@gmail.com>
2025-09-25 00:02:35 +0530

    updated

--- src/app/auction/edit/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-// import AuctionForm from '@/components/AuctionViewForm'
-
-// interface Props {
-//   params: { id: string }
-// }
-
-// export default function EditAuctionPage({ params }: Props) {
-//   return <AuctionForm mode="edit" auctionId={params.id} />
-// }
